43f05d46cab68f3d7b451dd81478bf05c7d1086d
KurnakovMaksim <maksk210@gmail.com>
2020-08-28 17:19:02 +0900

    Added initial files (Domain, Tests, WebUI)

---
+++ DeviceStore.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DeviceStore.WebUI.MvcApplication" Language="C#" %>
---
+++ DeviceStore.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DeviceStore.WebUI.Startup))]
+namespace DeviceStore.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
