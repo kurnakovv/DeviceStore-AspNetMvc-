57cc88c467d2dd36eea4bfae8b9591c9604d3215
KurnakovMaksim <maksk210@gmail.com>
2020-08-29 18:30:23 +0900

    Added device model

---
+++ DeviceStore.Domain/Entities/Device.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace DeviceStore.Domain.Entities
+{
+    public class Device
+    {
+        [HiddenInput(DisplayValue = false)]
+        public int DeviceId { get; set; }
+
+        [Required(ErrorMessage = "Укажите название устройства!")]
+        [Display(Name = "Название")]        
+        public string DeviceName { get; set; }
+
+        [Required(ErrorMessage = "Укажите описание устройства!")]
+        [Display(Name = "Описание")]
+        [DataType(DataType.MultilineText)]
+        public string DeviceDescription { get; set; }
+
+        [Required(ErrorMessage = "Укажите категорию устройства!")]
+        [Display(Name = "Категория")]
+        public string DeviceCategory { get; set; }
+
+        [Required(ErrorMessage = "Укажите количество устройств!")]
+        [Display(Name = "Количество")]
+        public int DeviceQuantity { get; set; }   
+        
+        [Display(Name = "Закладка")]
+        public bool DeviceFavorites { get; set; }
+
+        [Required(ErrorMessage = "Укажите цену устройства!")]
+        [Display(Name = "Цена")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Пожалуйста, введите положительное значение для цены!")]
+        public decimal DevicePrice { get; set; }
+
+        [Display(Name = "Фотография")]
+        public string DeviceImage { get; set; }
+    }
+}
