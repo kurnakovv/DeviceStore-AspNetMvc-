1394029f6d762cbd8b7a2e9db84ab59e92911ce8
KurnakovMaksim <maksk210@gmail.com>
2020-09-01 15:51:55 +0900

    Added database context with connection string

---
+++ DeviceStore.Domain/EFConcrete/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DeviceStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeviceStore.Domain.EFConcrete
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Device> Devices { get; set; }
+    }
+}
