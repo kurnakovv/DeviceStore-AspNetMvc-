ac36016d806afa136bfcd7cab77b97a6997286db
KurnakovMaksim <maksk210@gmail.com>
2020-09-01 16:18:01 +0900

    Added initial output of all devices to the main page

--- DeviceStore.WebUI/Controllers/HomeController.cs
+++ DeviceStore.WebUI/Controllers/HomeController.cs
@@ -1,16 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using DeviceStore.Domain.AbstractModel;
 using System.Web.Mvc;
 
 namespace DeviceStore.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        private IDeviceRepository _deviceRepository;
+
+        public HomeController(IDeviceRepository deviceRepository)
+        {
+            _deviceRepository = deviceRepository;
+        }
+
         public ActionResult Index()
         {
-            return View();
+            return View(_deviceRepository.Devices);
         }
 
         public ActionResult About()
