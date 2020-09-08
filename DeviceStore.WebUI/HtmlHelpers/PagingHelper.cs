589c02de3532fba4cfd13eb9b11c1c94a945a9bd
KurnakovMaksim <maksk210@gmail.com>
2020-09-08 17:11:54 +0900

    implemented html helper for generation html code for pagination

---
+++ DeviceStore.WebUI/HtmlHelpers/PagingHelper.cs
@@ -0,0 +1,35 @@
+﻿using DeviceStore.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DeviceStore.WebUI.HtmlHelpers
+{
+    public static class PagingHelper
+    {
+        public static MvcHtmlString GeneratesHtmlForPageLinks(this HtmlHelper html,
+                                              PagingInfo pagingInfo,
+                                              Func<int, string> pageUrl)
+        {
+            StringBuilder result = new StringBuilder();
+            for(int i = 1; i <= pagingInfo.TotalPages; i++)
+            {
+                TagBuilder tagBuilder = new TagBuilder("a");
+                tagBuilder.MergeAttribute("href", pageUrl(i));
+                tagBuilder.InnerHtml = i.ToString();
+                if(i == pagingInfo.CurrentPage)
+                {
+                    tagBuilder.AddCssClass("selected");
+                    tagBuilder.AddCssClass("btn btn-warning");
+                }
+                tagBuilder.AddCssClass("btn btn-default");
+                result.Append(tagBuilder.ToString());                
+            }
+
+            return MvcHtmlString.Create(result.ToString());
+        }
+    }
+}
