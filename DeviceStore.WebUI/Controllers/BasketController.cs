e0ddfdd390ad358d2224d2b49bb3214af00cb93a
KurnakovMaksim <maksk210@gmail.com>
2020-09-25 19:09:10 +0900

    Added initial Basket UI

---
+++ DeviceStore.WebUI/Controllers/BasketController.cs
@@ -0,0 +1,48 @@
+﻿using DeviceStore.Domain.Services.Interfaces;
+using DeviceStore.Domain.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DeviceStore.WebUI.Controllers
+{
+    public class BasketController : Controller
+    {
+        IBasketService _basketService;
+        
+        public BasketController(IBasketService basketService)
+        {
+            _basketService = basketService;
+        }
+
+        public ActionResult Index()
+        {
+            List<BasketItemViewModel> model = _basketService.GetBasketItems(HttpContext);
+            
+            return View(model);
+        }
+
+        public ActionResult AddToBasket(string id)
+        {
+            _basketService.AddToBasket(HttpContext, id);
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult RemoveFromBasket(string id)
+        {
+            _basketService.RemoveFromBasket(HttpContext, id);
+
+            return RedirectToAction("Index");
+        }
+
+        public PartialViewResult BasketSummary()
+        {
+            BasketSummaryViewModel basketSummary = _basketService.GetBasketSummary(HttpContext);
+
+            return PartialView("BasketSummary", basketSummary);
+        }
+    }
+}
