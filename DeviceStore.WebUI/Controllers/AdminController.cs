8c65f15a9a617141047fa24f8dba923d95d23b29
KurnakovMaksim <maksk210@gmail.com>
2020-10-08 17:52:46 +0900

    Added initial implementation GRUD for admin (devices)

---
+++ DeviceStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,102 @@
+﻿using DeviceStore.Domain.AbstractModel;
+using DeviceStore.Domain.Entities;
+using DeviceStore.Domain.Services;
+using DeviceStore.Domain.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DeviceStore.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly IDeviceRepository _deviceRepository;
+        private readonly IAdminService _adminService;
+
+        public AdminController(IDeviceRepository deviceRepository,
+                               IAdminService adminService)
+        {
+            _deviceRepository = deviceRepository;
+            _adminService = adminService;
+        }
+
+        public ViewResult Index()
+        {
+            return View(_deviceRepository.Devices);
+        }
+
+        public ViewResult EditDevice(Device device, string id)
+        {
+            device = _deviceRepository.Devices
+               .FirstOrDefault(d => d.Id == id);
+
+            return View(device);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditDevice(Device device, HttpPostedFileBase deviceImage = null)
+        {
+            if (ModelState.IsValid)
+            {
+                if (deviceImage != null)
+                {
+                    AddImage(device, deviceImage);
+                }
+
+                _adminService.EditDevice(device);
+                TempData["messageEdit"] = string.Format("Изменения в устройстве \"{0}\" были сохранены", device.DeviceName);
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(device);
+            }
+        }
+
+        public ActionResult DetailsDevice(string id)
+        {
+            Device device = _deviceRepository.Devices.FirstOrDefault(d => d.Id == id);
+            return PartialView(device);
+        }
+
+        public ViewResult CreateDevice()
+        {
+            return View("EditDevice", new Device());
+        }
+
+        public ActionResult RemoveDevice(Device device, string id)
+        {
+            device = _deviceRepository.Devices.FirstOrDefault(d => d.Id == id);
+
+            return PartialView(device);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult RemoveDevice(string id)
+        {
+            _adminService.RemoveDevice(id);
+            TempData["messageRemove"] = string.Format("Устройство было удалено успешно");
+
+            return RedirectToAction("Index");
+        }
+
+        private void AddImage(Device device, HttpPostedFileBase deviceImage)
+        {
+            if (deviceImage != null)
+            {
+                var fileName = Path.GetFileName(deviceImage.FileName);
+                var directoryToSave = Server.MapPath(Url.Content("~/Content/ProductImages/"));
+
+                var pathToSave = Path.Combine(directoryToSave, fileName);
+                deviceImage.SaveAs(pathToSave);
+                device.DeviceImage = fileName;
+            }
+        }
+    }
+}
---
+++ DeviceStore.WebUI/Scripts/window.js
@@ -0,0 +1,23 @@
+﻿$(function () {
+    $.ajaxSetup({ cache: false });
+    $(".deviceEdit").click(function (e) {
+
+        e.preventDefault();
+        $.get(this.href, function (data) {
+            $('#dialogContent').html(data);
+            $('#modelDialog').modal('show');
+        });
+    });
+})
+
+$(function () {
+    $.ajaxSetup({ cache: false });
+    $(".deviceDelete").click(function (d) {
+
+        d.preventDefault();
+        $.get(this.href, function (data) {
+            $('#dialogContent').html(data);
+            $('#modelDialog').modal('show');
+        });
+    });
+})
